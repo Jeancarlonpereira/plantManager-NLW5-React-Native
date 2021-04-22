8a2dd45c50107a646f32269ec461b6a01b1f91e3
Jean Pereia <jeancarlo.npereira@gmail.com>
2021-04-21 22:28:33 -0300

    finish day 2 - navigation and initial pages

---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {} from 'react-native'
+import { createStackNavigator } from "@react-navigation/stack";
+import colors from '../styles/colors'
+import { Welcome } from '../pages/Welcome';
+import { UserIdentification } from '../pages/UserIdentification';
+import { Confirmation } from '../pages/Confirmation';
+
+
+const stackRoute = createStackNavigator();
+
+const AppRoutes: React.FC = () =>(
+    <stackRoute.Navigator
+    headerMode='none'
+    screenOptions={{
+        cardStyle: {
+            backgroundColor: colors.white,
+        }
+    }}
+    >
+    <stackRoute.Screen
+    name="Welcome"
+    component={Welcome}
+    />
+        <stackRoute.Screen
+    name="UserIdentification"
+    component={UserIdentification}
+    />
+        <stackRoute.Screen
+    name="Confirmation"
+    component={Confirmation}
+    />
+
+
+    </stackRoute.Navigator>
+)
+
+export default AppRoutes;
